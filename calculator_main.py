231abcdf7962ca654dc3da0de994edcf601277ff
gbdngb12 <gbdngb12@naver.com>
2022-12-01 22:41:46 +0900

    UI Improved Like Windows calculator

--- calculator_main.py
+++ calculator_main.py
@@ -13,9 +13,8 @@ class Main(QDialog):
         self.operator = ""
 
         ### 각 위젯을 배치할 레이아웃을 미리 만들어 둠
-        layout_operation = QHBoxLayout()
-        layout_operation2 = QHBoxLayout()
-        layout_clear_equal = QHBoxLayout()
+
+        layout_equation = QGridLayout()
         layout_number = QGridLayout()
         layout_equation_solution = QFormLayout()
 
@@ -38,6 +37,7 @@ class Main(QDialog):
         button_reciprocal = QPushButton("1/x")
         button_square = QPushButton("x²")
         button_root = QPushButton("√x")
+        button_negative = QPushButton("+/-")
 
 
 
@@ -53,18 +53,10 @@ class Main(QDialog):
         button_reciprocal.clicked.connect(lambda state, operation = "1/x": self.button_operation_clicked(operation))
         button_square.clicked.connect(lambda state, operation = "square": self.button_operation_clicked(operation))
         button_root.clicked.connect(lambda state, operation = "root": self.button_operation_clicked(operation))
+        button_negative.clicked.connect(lambda state, operation = "neg": self.button_operation_clicked(operation))
 
         ### 사칙연산 버튼을 layout_operation 레이아웃에 추가
-        layout_operation.addWidget(button_plus)
-        layout_operation.addWidget(button_minus)
-        layout_operation.addWidget(button_product)
-        layout_operation.addWidget(button_division)
-        
 
-        layout_operation2.addWidget(button_remainder)
-        layout_operation2.addWidget(button_reciprocal)
-        layout_operation2.addWidget(button_square)
-        layout_operation2.addWidget(button_root)
         ### =, clear, backspace 버튼 생성
         button_equal = QPushButton("=")
         button_clearall = QPushButton("C")
@@ -77,11 +69,6 @@ class Main(QDialog):
         button_clear.clicked.connect(self.button_clear_clicked)
         button_backspace.clicked.connect(self.button_backspace_clicked)
 
-        ### =, clear, backspace 버튼을 layout_clear_equal 레이아웃에 추가
-        layout_clear_equal.addWidget(button_clearall)
-        layout_clear_equal.addWidget(button_clear)
-        layout_clear_equal.addWidget(button_backspace)
-        layout_clear_equal.addWidget(button_equal)
 
         ### 숫자 버튼 생성하고, layout_number 레이아웃에 추가
         ### 각 숫자 버튼을 클릭했을 때, 숫자가 수식창에 입력 될 수 있도록 시그널 설정
@@ -99,17 +86,26 @@ class Main(QDialog):
         ### 소숫점 버튼과 00 버튼을 입력하고 시그널 설정
         button_dot = QPushButton(".")
         button_dot.clicked.connect(lambda state, num = ".": self.number_button_clicked(num))
-        layout_number.addWidget(button_dot, 3, 2)
+        layout_equation.addWidget(button_remainder, 0, 0)
+        layout_equation.addWidget(button_clearall, 0, 1)
+        layout_equation.addWidget(button_clear, 0, 2)
+        layout_equation.addWidget(button_backspace,0, 3)
+        layout_equation.addWidget(button_reciprocal, 1, 0)
+        layout_equation.addWidget(button_square, 1, 1)
+        layout_equation.addWidget(button_root, 1, 2)
+        layout_equation.addWidget(button_division, 1, 3)
 
-        button_double_zero = QPushButton("00")
-        button_double_zero.clicked.connect(lambda state, num = "00": self.number_button_clicked(num))
-        layout_number.addWidget(button_double_zero, 3, 0)
+        layout_number.addWidget(button_product,0,3)
+        layout_number.addWidget(button_minus,1,3)
+        layout_number.addWidget(button_plus,2,3)
+        layout_number.addWidget(button_equal, 3, 3)
+        layout_number.addWidget(button_dot, 3, 2)
+        layout_number.addWidget(button_negative,3,0)
+
 
         ### 각 레이아웃을 main_layout 레이아웃에 추가
         main_layout.addLayout(layout_equation_solution)
-        main_layout.addLayout(layout_operation)
-        main_layout.addLayout(layout_operation2)
-        main_layout.addLayout(layout_clear_equal)
+        main_layout.addLayout(layout_equation)
         main_layout.addLayout(layout_number)
 
         self.setLayout(main_layout)
@@ -122,9 +118,12 @@ class Main(QDialog):
         equation = self.equation.text()
         equation += str(num)
         self.equation.setText(equation)
-
+    
     def button_operation_clicked(self, operation):
-        #self.answer = self.equation.text()
+        if operation == "neg" :
+            saved = self.equation.text()
+            self.equation.setText("-" + saved)
+            return
         if len(self.operator) == 0 :
             self.operator = operation
             self.answer = self.equation.text()
@@ -160,7 +159,6 @@ class Main(QDialog):
             self.equation.setText(str(float(self.answer) + float(self.equation.text())))
         elif self.operator == '-' :
             self.equation.setText(str(float(self.answer) - float(self.equation.text())))
-
         elif self.operator == '*' :
             self.equation.setText(str(float(self.answer) * float(self.equation.text())))
         elif self.operator == '/' :
